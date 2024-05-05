5049021f7fcc821e5ffb3ef5581b783f4e309dea
devkevbot <31908183+devkevbot@users.noreply.github.com>
2024-05-05 14:03:42 -0700

    refactor: change Game methods to be less verbose

--- internal/app/models.go
+++ internal/app/models.go
@@ -12,6 +12,36 @@ type DailyScores struct {
 	Games []Game `json:"games"`
 }
 
+func (ds *DailyScores) String() string {
+	if len(ds.Games) == 0 {
+		return "No game data was available for the requested date(s)."
+	}
+
+	return ds.toTable()
+}
+
+func (ds *DailyScores) toTable() string {
+	t := table.NewWriter()
+	t.SuppressEmptyColumns()
+
+	t.AppendHeader(table.Row{
+		"GAME TYPE",
+		"PLAYOFF GAME",
+		"PLAYOFF SERIES",
+		"START TIME",
+		"TEAMS",
+		"SCORE",
+		"STATUS",
+		"GAME-WINNING GOAL",
+	})
+
+	for _, game := range ds.Games {
+		t.AppendRow(game.toRow())
+	}
+
+	return t.Render()
+}
+
 type Name struct {
 	Default string `json:"default"`
 }
@@ -114,45 +144,20 @@ type Game struct {
 	Goals             []Goal           `json:"goals"`
 }
 
-func (ds *DailyScores) String() string {
-	if len(ds.Games) == 0 {
-		return "No game data was available for the requested date(s)."
-	}
-
-	t := table.NewWriter()
-	t.AppendHeader(table.Row{
-		"GAME TYPE",
-		"PLAYOFF GAME",
-		"PLAYOFF SERIES",
-		"START TIME",
-		"TEAMS",
-		"SCORE",
-		"STATUS",
-		"GAME-WINNING GOAL",
-	})
-	t.SuppressEmptyColumns()
-
-	for _, game := range ds.Games {
-		t.AppendRow(game.toRow())
-	}
-
-	return t.Render()
-}
-
 func (g *Game) toRow() table.Row {
 	return table.Row{
-		g.getGameTypeName(),
-		g.getPlayoffGameNumber(),
-		g.getPlayoffSeriesStatus(),
-		g.getUserLocalStartTime(),
-		g.getTeams(),
-		g.getScore(),
-		g.getStatus(),
-		g.getGameWinningGoal(),
+		g.gameTypeCol(),
+		g.playoffGameCol(),
+		g.playoffSeriesCol(),
+		g.startTimeCol(),
+		g.teamsCol(),
+		g.scoreCol(),
+		g.statusCol(),
+		g.gwgCol(),
 	}
 }
 
-func (g *Game) getGameTypeName() string {
+func (g *Game) gameTypeCol() string {
 	gameTypeToName := map[int]string{
 		1: "Preseason",
 		2: "Regular Season",
@@ -161,41 +166,79 @@ func (g *Game) getGameTypeName() string {
 	return gameTypeToName[g.GameType]
 }
 
-func (g *Game) getPlayoffGameNumber() string {
-	if g.SeriesStatus.Round < 1 {
+func (g *Game) isPlayoffGame() bool {
+	return g.SeriesStatus.Round >= 1
+}
+
+func (g *Game) playoffGameCol() string {
+	if !g.isPlayoffGame() {
 		return ""
 	}
+
 	return fmt.Sprintf("R%d, GM %d", g.SeriesStatus.Round, g.SeriesStatus.GameNumberOfSeries)
 }
 
-func (g *Game) getUserLocalStartTime() string {
+func (g *Game) playoffSeriesCol() string {
+	if !g.isPlayoffGame() {
+		return ""
+	}
+
+	// Series is tied
+	if g.SeriesStatus.TopSeedWins == g.SeriesStatus.BottomSeedWins {
+		return fmt.Sprintf("TIED %d-%d", g.SeriesStatus.TopSeedWins, g.SeriesStatus.BottomSeedWins)
+	}
+	// Top seed is leading
+	if g.SeriesStatus.TopSeedWins > g.SeriesStatus.BottomSeedWins {
+		return fmt.Sprintf("%s %d-%d", g.SeriesStatus.TopSeedTeamAbbrev, g.SeriesStatus.TopSeedWins, g.SeriesStatus.BottomSeedWins)
+	}
+	// Bototm seed is leading
+	return fmt.Sprintf("%s %d-%d", g.SeriesStatus.BottomSeedTeamAbbrev, g.SeriesStatus.BottomSeedWins, g.SeriesStatus.TopSeedWins)
+}
+
+func (g *Game) startTimeCol() string {
 	return g.StartTimeUTC.Local().Format("02 Jan 06 15:04 MST")
 }
 
-func (g *Game) getAwayTeamAbbrev() string {
+func (g *Game) awayAbbrev() string {
 	return g.AwayTeam.Abbrev
 }
 
-func (g *Game) getAwayTeamScore() int {
+func (g *Game) awayScore() int {
 	return g.AwayTeam.Score
 }
 
-func (g *Game) getHomeTeamAbbrev() string {
+func (g *Game) homeAbbrev() string {
 	return g.HomeTeam.Abbrev
 }
 
-func (g *Game) getHomeTeamScore() int {
+func (g *Game) homeScore() int {
 	return g.HomeTeam.Score
 }
 
-func (g *Game) getTeams() string {
-	awayAbbrev := g.getAwayTeamAbbrev()
-	homeAbbrev := g.getHomeTeamAbbrev()
+func (g *Game) teamsCol() string {
+	awayAbbrev := g.awayAbbrev()
+	homeAbbrev := g.homeAbbrev()
 	team := awayAbbrev + " at " + homeAbbrev
 	return team
 }
 
-func (g *Game) getStatus() string {
+func (g *Game) scoreCol() string {
+	awayScore := g.awayScore()
+	homeScore := g.homeScore()
+	awayAbbrev := g.awayAbbrev()
+	homeAbbrev := g.homeAbbrev()
+
+	if awayScore > homeScore {
+		return fmt.Sprintf("%d-%d %s", awayScore, homeScore, awayAbbrev)
+	}
+	if homeScore > awayScore {
+		return fmt.Sprintf("%d-%d %s", homeScore, awayScore, homeAbbrev)
+	}
+
+	return fmt.Sprintf("%d-%d", awayScore, homeScore)
+}
+
+func (g *Game) statusCol() string {
 	gameStateToDesc := map[string]string{
 		"LIVE":  "In-progress",
 		"FUT":   "Not started",
@@ -223,38 +266,8 @@ func (g *Game) getStatus() string {
 	return strings.Join(output, " ")
 }
 
-func (g *Game) getScore() string {
-	awayScore := g.getAwayTeamScore()
-	homeScore := g.getHomeTeamScore()
-	awayAbbrev := g.getAwayTeamAbbrev()
-	homeAbbrev := g.getHomeTeamAbbrev()
-
-	if awayScore > homeScore {
-		return fmt.Sprintf("%d-%d %s", awayScore, homeScore, awayAbbrev)
-	}
-	if homeScore > awayScore {
-		return fmt.Sprintf("%d-%d %s", homeScore, awayScore, homeAbbrev)
-	}
-
-	return fmt.Sprintf("%d-%d", awayScore, homeScore)
-}
-
-func (g *Game) getPlayoffSeriesStatus() string {
-	// Round 0 is non-playoff hockey
-	if g.SeriesStatus.Round < 1 {
-		return ""
-	}
-	if g.SeriesStatus.TopSeedWins == g.SeriesStatus.BottomSeedWins {
-		return fmt.Sprintf("TIED %d-%d", g.SeriesStatus.TopSeedWins, g.SeriesStatus.BottomSeedWins)
-	}
-	if g.SeriesStatus.TopSeedWins > g.SeriesStatus.BottomSeedWins {
-		return fmt.Sprintf("%s %d-%d", g.SeriesStatus.TopSeedTeamAbbrev, g.SeriesStatus.TopSeedWins, g.SeriesStatus.BottomSeedWins)
-	}
-	return fmt.Sprintf("%s %d-%d", g.SeriesStatus.BottomSeedTeamAbbrev, g.SeriesStatus.BottomSeedWins, g.SeriesStatus.TopSeedWins)
-}
-
-func (g *Game) getGameWinningGoal() string {
-	if g.getStatus() != "FINAL" {
+func (g *Game) gwgCol() string {
+	if g.statusCol() != "FINAL" {
 		return ""
 	}
 
@@ -263,8 +276,8 @@ func (g *Game) getGameWinningGoal() string {
 	}
 
 	var gwg Goal
-	awayScore := g.getAwayTeamScore()
-	homeScore := g.getHomeTeamScore()
+	awayScore := g.awayScore()
+	homeScore := g.homeScore()
 	if awayScore > homeScore {
 		for _, goal := range g.Goals {
 			if goal.AwayScore == homeScore+1 {
